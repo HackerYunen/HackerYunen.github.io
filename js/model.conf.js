796c21ec26da9a0e921c04bce2ab72b0c18545d1
Yunen <2865859175@qq.com>
2019-06-19 00:06:41 +0800

    Site updated: 2019-06-19 00:06:39

--- js/model.conf.js
+++ js/model.conf.js
@@ -1,15 +1,16 @@
 var canvas = $("<canvas>");
 canvas.attr('id','mycanvas');
-canvas.attr('width','300');
-canvas.attr('height','500');
-canvas.attr('right','330');
-canvas.attr('buttom','530');
-canvas.attr('style','border:dashed 1px #CCC;');
+canvas.attr('width','200');
+canvas.attr('height','400');
+canvas.attr('style','position: fixed; opacity: 0.7; right: 50px; bottom: -20px; z-index: 99999;');
 $("#content-outer").append(canvas); 
+
+$("#cola").attr('onclick','color()');
+$("#shutiao").attr('onclick','colorAndCrisps()');
 
 var LAppDefine = {   
 	// 调试，true时会在console里显示日志
-	DEBUG_LOG : true,
+	DEBUG_LOG : false,
 	DEBUG_MOUSE_LOG : false, // 鼠标相关日志   
 	//  全部设定    
 	//这里配置canvsa元素的id
@@ -17,9 +18,9 @@ var LAppDefine = {
 	//是否开启滚轮缩放，默认true
 	IS_SCROLL_SCALE : true,
 	// 画面最大缩放级别
-	VIEW_MAX_SCALE : 2,
+	VIEW_MAX_SCALE : 1,
 	// 画面最小缩放级别
-	VIEW_MIN_SCALE : 0.8,
+	VIEW_MIN_SCALE : 1,
 
 	VIEW_LOGICAL_LEFT : -1,
 	VIEW_LOGICAL_RIGHT : 1,
@@ -60,7 +61,7 @@ var LAppDefine = {
 	*/
 	MODELS:
 	[
-		["model/xiaomai/xiaomai.model.json"]
+		["/model/xiaomai/xiaomai.model.json"]
 	],
 
 	// 与外部定义的json文件匹配
@@ -90,4 +91,15 @@ function subSomething()
 	if(document.readyState =='complete'){ //当页面加载状态 
 		InitLive2D();
 	}
+}
+
+
+function color() {
+	var model = new LAppModel();
+	model.startAppointMotion("start",3,3)
+}
+
+function colorAndCrisps() {
+	var model = new LAppModel();
+	model.startAppointMotion("start",3,6)
 }
