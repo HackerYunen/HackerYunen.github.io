626756419374965191078bc71e94f15b9c0bbf3e
Yunen <2865859175@qq.com>
2020-12-25 06:02:12 +0800

    Site updated: 2020-12-25 06:02:01

---
+++ js/night.js
@@ -0,0 +1,41 @@
+(function () {
+    /**
+     * Icarus 夜间模式 by iMaeGoo
+     * https://www.imaegoo.com/
+     */
+    var isNight = localStorage.getItem('night');
+    var nightNav;
+
+    function applyNight(value) {
+        if (value.toString() === 'true') {
+            document.body.classList.add('night');
+        } else {
+            document.body.classList.remove('night');
+        }
+    }
+
+    function findNightNav() {
+        nightNav = document.getElementById('night-nav');
+        if (!nightNav) {
+            setTimeout(findNightNav, 100);
+        } else {
+            nightNav.addEventListener('click', switchNight);
+        }
+    }
+
+    function switchNight() {
+        isNight = isNight ? isNight.toString() !== 'true' : true;
+        applyNight(isNight);
+        localStorage.setItem('night', isNight);
+    }
+
+    findNightNav();
+
+    let h = (new Date()).getHours();
+    if ((h >= 18 || h <= 6) && (isNight == undefined || isNight.toString() !== 'true')) {
+        switchNight();
+    }else {
+        isNight && applyNight(isNight);
+    }
+
+}());
